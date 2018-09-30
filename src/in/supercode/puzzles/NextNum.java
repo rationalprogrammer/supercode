423deb4d564a7cacfcd0bef5b39b8b56371a2ebd
Arav <Arav@DESKTOP-NCR41T7>
2018-09-30 14:32:11 +0530

    Completed the logic

--- src/in/supercode/puzzles/NextNum.java
+++ src/in/supercode/puzzles/NextNum.java
@@ -1,7 +1,6 @@
 package in.supercode.puzzles;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * Choose any 5 digits
@@ -9,78 +8,165 @@ import java.util.List;
  * Given a number made from those 5 digits (may NOT be unique), find what should be 
  * the next number
  * 
- * 1. test case - find all numbers and sort them. Use them to test
+ * The algo
+ * 1. Check if the numbers are in descending order, if yes, then you have found the answer.
+ * 2. If no find a pivot number and then arrange in ascending order the next set
  * 
  * @author Gaurav
  *
  */
 public class NextNum {
 	
-	
+	// only positive integers
 	private static int next(int number) {
 		
+		// find the number
 		String sn = ""+number;
-		int[] nArr = new int[sn.length()];
+		// split the numbers into int array
+		int[] digits = new int[sn.length()];	
+		char[] ca = sn.toCharArray(); 
+		for (int i=0; i<sn.length(); i++) {			
+			digits[i] = Integer.parseInt(""+ca[i]);
+		}
+				
+		// if descending then return same array
+		if (isDesc(digits))
+			return number;
 		
-		return 0;
+		// else find the pivot from which the number is descending
+		int pin = findPivotIndex(digits);
+		// check. Should not come here as we have already made a check above
+		// nevertheless
+		if (pin == -1) {
+			return number;
+		}
+		
+		// once you get the pivot index
+		// 1. split the array into 2 parts
+		// 2. then find the next number for the second half
+		
+		int[] firstHalf =  copy(digits, 0, pin-1);
+		int[] secondHalf = copy(digits, pin, digits.length-1);
+		
+		// the second half must then be arranged in sorted order
+		int[] sortedSecHalf = sortByNextBigNum(secondHalf);
+		
+		// combine the two arrays and get the number
+		int[] numArr = combine(firstHalf, sortedSecHalf);
+		
+		return convert(numArr) ;
+		
 	}
 	
+	private static int[] combine(int[] arr1, int[] arr2) {
+		int[] arr = new int[arr1.length+arr2.length];
+		for (int i=0; i<arr1.length; i++) {
+			arr[i] = arr1[i];
+		}
+		for (int i=0; i<arr2.length; i++) {
+			arr[i+arr1.length] = arr2[i];
+		}
+		return arr;
+	}
+	
+	// the number is scanned from right to left
+	private static int findPivotIndex(int... iArr) {
+		// start last but 1
+		for (int i=iArr.length-2; i>=0; i--) {
+			if (iArr[i] < iArr[i+1]) 
+				return i;			
+		}		
+		// nothing found? return -1
+		// it tells it is sorted in descending order
+		return -1;
+	}
+	
+	// copy from index to index both inclusive
+	private static int[] copy(int[] iArr, int fromIndex, int toIndex) {
+		int[] ia = new int[toIndex-fromIndex+1];
+		for (int i=fromIndex, j=0; i<=toIndex; i++, j++) {
+			ia[j] = iArr[i];
+		}
+		return ia;
+	}
+			
+	private static int convert(int... digits) {
+		int factor = 1;
+		int num = 0;
+		for (int i=digits.length-1; i>=0; i--) {
+			num += digits[i]*factor;
+			factor *= 10;
+		}
+		return num;
+	}
+	
+	/**
+	 * Take out the first number and see what is the biggest number in that array
+	 * put that number in front and put the remaining numbers in ascending order. 
+	 * 
+	 * @param iArr
+	 * @return
+	 */
+	private static int[] sortByNextBigNum(int... iArr) {
+		
+		// 1. find the first num
+		// 2. sort the array
+		// 3. see which index is bigger than the first array
+		// 4. swap with that number
+		int firstNum = iArr[0];
+		Arrays.sort(iArr);
+		int bigIndex = -1;
+		
+		for (int i=0; i<iArr.length; i++) {
+			if (iArr[i] > firstNum) {
+				bigIndex = i;
+				break;
+			}			
+		}
+		
+		// it is already sorted
+		if (bigIndex == -1)
+			return iArr;
+		
+		// swap with first number
+		int temp = iArr[bigIndex];
+		iArr[bigIndex] = iArr[0];
+		iArr[0] = temp;	
+		
+		return iArr;
+	}
+	
+	// check if the values of the array are in descending
+	// this gives the largest number
+	private static boolean isDesc(int... iArr) {
+		//choose the largest number
+		int prev = iArr[0];				
+		// ignore the first number, start with first
+		for (int i=1; i<iArr.length; i++) {
+			if (iArr[i] > prev)
+				return false;
+			else 
+				prev = iArr[i];
+		}
+		return true;
+	}
+
+	private static int print(int num) {
+		int nextNum = next(num);
+		System.out.println(num + "->" + nextNum);
+		return nextNum;
+	}
+	
+	//private static int
 	
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-//		TestNextNum tnn = new TestNextNum(23456);
-//		tnn.testFact();
-	}
-
-}
-
-/**
- * The test class for testing the numbers.
- * This will also find all numbers that can be created by
- * scrambling a number. Just sort them after that
- * 
- * @author Gaurav 
- *
- */
-class TestNextNum {
-	
-	// > 2 digits but <= 6
-	private int baseNum;
-	private int[] allNum;
-	
-	private static int fact(int num) {
-		if (num <= 1)
-			return 1;
-		else {
-			int f = num;
-			while (num > 1) {
-				f *= (--num);
-			}
-			return f;
+		int start = 138;
+		//Arrays.	
+		for (int i=0; i<4; i++) {
+			 start = print(start);
 		}
 	}
 	
-	// constructor
-	public TestNextNum(int baseNum) {
-		// get the length, and create an array
-		//allNum = new int[fact((""+baseNum).length())];
-		List<Integer> iList = new ArrayList<>(100);
-		
-		// scramble and add to list
-	}
-	
-	public void testFact() {
-		System.out.println(fact(0));
-		System.out.println(fact(1));
-		System.out.println(fact(2));
-		System.out.println(fact(3));
-		System.out.println(fact(4));
-		System.out.println(fact(5));
-		System.out.println(fact(6));
-	}	
-	
 }
+
+
